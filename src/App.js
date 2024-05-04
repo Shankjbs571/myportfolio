8b804a431c81c7988ae672364665c98f48ec970d
Shashank Kumar <shankjbs571@gmail.com>
2024-05-04 23:44:18 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import Image from './components/Image.js';
+import React from 'react';
+// import resume from './Neil-Purohit.pdf';
+import Theme from './Theme.js';
+import Skills from './components/Skills.js';
+import photo from './assets/twitter.png';
+import Sidebar from './components/Sidebar.js';
+
+export default function App() {
+  document.body.classList.add("bg-white", "dark:bg-black");
+  const resume = () => { alert("I will Upload soon"); };
+  return (
+    <div className='grid sm:grid-cols-4 gap-4'>
+        <Sidebar/>
+        <div className="h-screen flex  items-center mt-32 flex-col col-span-3">
+          
+          <div className='flex flex-col '>
+          <Image/>
+            <div className="flex justify-centre">
+                
+                <div className="lg:mt-0 inline-grid text-center lg:text-left w-full gap-3 lg:gap-1">
+                  <p className="mt-20 md:mt-0 text-6xl font-mono font-bold text-black dark:text-white">Shashank Kumar</p>
+                  <div className="inline-grid lg:flex lg:items-center gap-3">
+                  <div className="flex flex-row text-black dark:text-white gap-3 justify-center">
+                    <a href="https://github.com/Shankjbs571" target="_blank" rel="noopener noreferrer">
+                      <img src="https://skillicons.dev/icons?i=github" height="30" width="30" alt="Github" />
+                    </a>
+                    <a href="https://linkedin.com/in/Shankjbs571" target="_blank" rel="noopener noreferrer">
+                      <img src="https://skillicons.dev/icons?i=linkedin" height="30" width="30" alt="linkedin" />
+                    </a>
+                    <a href="https://github.com/Shankjbs571" target="_blank" rel="noopener noreferrer">
+                      <img src={photo} height="30" width="30" alt="Twitter" />
+                    </a>
+                    <a href="https://github.com/Shankjbs571" target="_blank" rel="noopener noreferrer">
+                      <img src="https://skillicons.dev/icons?i=gmail" height="30" width="30" alt="gmail" />
+                    </a>
+                    <Theme/>
+                  </div>
+
+
+                    
+                  </div>
+                  <div className="inline-grid lg:flex lg:items-center gap-3">
+                    <p className="font-mono text-sm text-black dark:text-white">Web • Data • AI • Design</p>
+                  </div>
+                </div>
+            </div>
+            <div className="flex flex-col items-center lg:items-start mx-12 lg:mx-0 mt-10 lg:w-5/6">
+              <p className="lg:mb-5 text-center lg:text-left text-md lg:text-xl font-mono font-medium text-black dark:text-white">
+              I study Computer Science and Engineering at the University of Lucknow and also Data Science and Applications from IIT Madras.
+              I can build things using Python, Django, Flask, SQL as well as NoSQL databases. 
+              I also enjoy working on ML projects and data science-related stuff. Additionally, I was a Django intern at CRIS Indian Railways. </p>
+                {/* <i>Data & Analytics Summer Intern</i>, and I am the 
+                <i>Founder</i> of 
+                <a className="hover:font-semibold" href="https://prospectiveprogramming.org"><u>ProspectiveProgramming</u></a> and 
+                <i>Co-Founder</i> of <a className="hover:font-semibold" href="https://futurepearstudios.co"><u>FuturePearStudios</u></a>. 
+                Currently, I am dual-enrolled in <i>CS 331: Data Structures and Algorithms</i> at the Illinois Institute of Technology for the Spring 2024 term.</p> */}
+            </div>
+          </div>
+         </div> 
+    </div>
+  );
+
+}
---
+++ src/components/Image.js
@@ -0,0 +1,14 @@
+import photo from '../assets/shashank.jpg';
+import React from 'react';
+
+export default function Image() {
+    var image = <img className=" rounded-full border-black dark:border-white" src={photo} height="200" width="200"></img>;
+    // if (window.innerWidth < 1024) {
+    //     image = "";
+    // }
+    return (
+        <div className=" max-lg:flex max-lg:justify-center">
+            <div>{image}</div>
+        </div>
+    );
+}
