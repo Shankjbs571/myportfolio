71246995df9e2a3bd5db37ceb40f41c95f782f92
Shashank Kumar <shankjbs571@gmail.com>
2024-05-05 17:08:33 +0530

    Restructured components

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,36 @@
 import React from 'react';
-
 export default function Skills() {
 
     return (
-        <div>
-            Skills
+        <div id='twosec' className='flex flex-col pt-10 pb-10 '>
+            <div className="flex justify-centre">
+
+              <div className="lg:mt-0 inline-grid text-center lg:text-left w-full gap-3 lg:gap-1">
+                <p className="mt-10 md:mt-0 text-6xl font-mono font-bold text-black dark:text-white">Skills</p>
+                <div className="inline-grid lg:flex lg:items-center gap-3">
+                  <div className="flex flex-row  gap-3 justify-center sm:justify-start w-full ">
+                    
+                      <img src="https://skillicons.dev/icons?i=django" height="40" width="40" alt="Github" />
+                      <img src="https://skillicons.dev/icons?i=flask" height="40" width="40" alt="Github" />
+                      <img src="https://skillicons.dev/icons?i=gcp" height="40" width="40" alt="Github" />
+                      <img src="https://skillicons.dev/icons?i=docker" height="40" width="40" alt="linkedin" />
+                      <img src="https://skillicons.dev/icons?i=react" height="40" width="40" alt="Twitter" />
+                      <img src="https://skillicons.dev/icons?i=py" height="40" width="40" alt="gmail" />
+                      <img src="https://skillicons.dev/icons?i=vue" height="40" width="40" alt="gmail" />
+                      <img src="https://skillicons.dev/icons?i=postman" height="40" width="40" alt="gmail" />
+                      
+                    
+                  </div>
+
+                    
+
+                </div>
+
+              </div>
+            </div>
+
         </div>
-    )
+    );
 
 
 }
